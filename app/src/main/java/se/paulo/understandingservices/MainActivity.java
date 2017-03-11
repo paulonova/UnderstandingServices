43560a40051d8a15da9aaf1bb94b3e7bf470ad75
paulonova <paulonova144@gmail.com>
2017-03-11 21:54:05 +0100

    Bound Service

--- app/src/main/java/se/paulo/understandingservices/MainActivity.java
+++ app/src/main/java/se/paulo/understandingservices/MainActivity.java
@@ -1,6 +1,10 @@
 package se.paulo.understandingservices;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,20 +13,25 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button startBtn, stopBtn;
+    Button startBtn, stopBtn, firstMsgBtn, secondMsgBtn;
     EditText getMessageEdt;
-    MyService myService;
+    MyBoundService myBoundService;
+    boolean isBind = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        myService = new MyService();
-
         startBtn = (Button)findViewById(R.id.button);
         stopBtn = (Button)findViewById(R.id.button2);
+        firstMsgBtn = (Button)findViewById(R.id.button3);
+        secondMsgBtn = (Button)findViewById(R.id.button4);
         getMessageEdt = (EditText)findViewById(R.id.editText);
+
+        Intent intent = new Intent(this, MyBoundService.class);
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
 
     }
 
@@ -38,12 +47,42 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-//    public void getMessage(View v){
-//        myService.testIfServiceWorks(getMessageEdt.getText().toString());
-//
-//    }
+
+    public void getFirstMessage(View v){
+        String message = myBoundService.getFirstMessage();
+        getMessageEdt.setText(message);
+
+    }
 
 
+    public void getSecondMessage(View v){
+        String message = myBoundService.getSecondMessage();
+        getMessageEdt.setText(message);
+    }
 
 
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder service) {
+
+            MyBoundService.LocalService localService = (MyBoundService.LocalService) service;
+            myBoundService = localService.getService();
+            isBind = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            isBind=false;
+        }
+    };
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(isBind){
+            unbindService(mConnection);
+            isBind=false;
+        }
+    }
 }
